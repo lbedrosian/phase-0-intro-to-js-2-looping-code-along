86a0339a6af9d21ea08e087e20104be3c769d103
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-08-12 11:46:32 -0400

    Completed Assignmant

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let newCards = [];
+    for (let i = 0; i < name.length; i++) {
+    newCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+return newCards;
+}
+function countDown(x) {
+    let startingInt = x;
+    while (x > -1) {
+    console.log(x--);
+    }
+}
